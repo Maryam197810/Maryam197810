08d32634ae37e4be24b0f108479debb0f6ceb41d
bashir mohammed itopa <120298170+Maryam197810@users.noreply.github.com>
2022-12-13 17:01:10 +0100

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- bashir mohammed itopa
+-  I’m interested in  this program 
+- working with ecowas commission
+- 💞️ I’m looking to collaborate with other learners 
+-  07034565221
+itopabashir2@gmail.com
+CEO BASH-AMNAB NIG LTD
+Maryam197810/Maryam197810 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
